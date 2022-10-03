700047142fd64fefb757837d57550562a0868384
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-10-03 22:53:36 +1100

    functional

--- add_procedural.py
+++ add_procedural.py
@@ -23,3 +23,20 @@ else:
        n1 = n2
        n2 = nth
        count += 1
+
+def my_function():
+    x = 25
+    y = 45
+
+    print('x + y =', x + y)
+
+    print('x - y =', x - y)
+
+    print('x * y =', x * y)
+
+    print('x / y =', x / y)
+
+    print('x // y =', x // y)
+
+    print('x ** y =', x ** y)
+    print("From here")
---
+++ main_function.py
@@ -0,0 +1,6 @@
+# Python program to use
+# main for function call.
+import add_procedural
+
+if __name__ == "__main__":
+    add_procedural.my_function()
