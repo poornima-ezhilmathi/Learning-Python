0048f954f2f496b728c9dcc93e2b12ff31b9c65e
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-10-12 10:27:01 +1100

    procedural and functional coding

--- add_functional.py
+++ add_functional.py
@@ -1,14 +1,17 @@
 def my_function(x,y):
 
     print('Function call')
-    print('x + y =', x + y)
+    if x>0 and y>0:
+        print('x + y =', x + y)
 
-    print('x - y =', x - y)
+        print('x - y =', x - y)
 
-    print('x * y =', x * y)
+        print('x * y =', x * y)
 
-    print('x / y =', x / y)
+        print('x / y =', x / y)
 
-    print('x // y =', x // y)
+        print('x // y =', x // y)
 
-    print('x ** y =', x ** y)
+        print('x ** y =', x ** y)
+    else:
+        print("Please input positive number")
