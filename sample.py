f7cda7f5c5607fb8659a86fb2545c85f194c0bb1
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-10-14 00:56:18 +1100

    leapyear

--- sample.py
+++ sample.py
@@ -1,6 +1,27 @@
-x = int(input())
-y = int(input())
 
-z = x-y
 
-print(z)
+def add_numbers(x, y):
+    x = input("Enter first value")
+    y = input("Enter Second Value")
+    if (int(x) <= 0 or int(y) <= 0):
+        print(" please enter a positive number")
+        exit()
+    else:
+        z = int(x) + int(y)
+        print(int(z))
+
+
+add_numbers(10, 20)
+
+
+def subtract_numbers(x,y):
+    x = input("Enter first value")
+    y = input("Enter Second Value")
+    if (int(x)<=0 or int(y)<=0):
+        print(" please enter a positive number")
+        exit()
+    else :
+        z = int(x) - int(y)
+        print(x,"-",y,"=",int(z))
+
+subtract_numbers(10,20)
