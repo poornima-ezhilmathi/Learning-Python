59087155f791a96fc1ee94ec38a54cdf9840df57
poornima-ezhilmathi <113603518+poornima-ezhilmathi@users.noreply.github.com>
2022-10-20 21:32:22 +1100

    Update

--- leap_year.py
+++ leap_year.py
@@ -3,15 +3,10 @@ def leap_year():
     print("Checking if",i, "is a Leap Year")
     if (i%400==0):
        print(i, "is a Leap Year as it's divisible by 400")
-       exit()
     elif (i%100==0):
-        if (i%400!=0):
-           print(i, "is a Leap Year as it's divisible by 100 but not by 400 ")
-        exit()
+           print(i, "is not a Leap Year as it's divisible by 100 but not by 400 ")
     elif (i%4==0):
-        if (i%400!=0) or (i%400!=0):
            print(i, "is a Leap Year as it's divisible by 4 but not by 100 or 400")
-           exit()
     else:
         print("Not a leap year")
 
