eb97b47a2748b2d9c86d944dbc7aabea9968e1fd
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-11-01 17:34:20 +1100

    string_slicing_operations

--- string_slicing_operations.py
+++ string_slicing_operations.py
@@ -3,18 +3,16 @@ def str_slicing_operations():
     str_val=input()
     print("You entered : '{0}'".format(str_val))
     print("--------------------------------------------")
-    print("Slice of  Character in 'Hello' is '{0}' " .format(str_val[0]))
+    print("Slice of '{1}' after removing first and last character '{0}' " .format(str_val[1:-1],str_val))
     print("--------------------------------------------")
-    print(len(str_val))
-    lst_str = len(str_val) -1
-    print("Last Character in 'Hello' is '{0}'" .format(str_val[lst_str]))
+    print("Slice of '{1}' after removing first 2 character '{0}' " .format(str_val[2:],str_val))
     print("--------------------------------------------")
-    print("Length Character in 'Hello' is  '{0}'" .format(len(str_val)))
+    print("Slice of '{1}' after removing  last 2 character '{0}' " .format(str_val[:-2],str_val))
     print("--------------------------------------------")
     mid_index=len(str_val)//2
     print("Mid index is '{0}'" .format(mid_index))
     print("--------------------------------------------")
-    # if the string is even then return two character otherwise one character
-    print("Middle Character in 'Hello' is/are {0}". format(str_val[(len(str_val)-1)//2:(len(str_val)+2)//2]))
+    print("First half slice of '{1}' is {0} is".format(str_val[:(len(str_val)) // 2],str_val))
+    print("Second half slice of '{1}' is '{0}'". format(str_val[(len(str_val))//2:],str_val))
 
 str_slicing_operations()
