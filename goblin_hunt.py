926f0035acd08a409519ea0bed13ce8263f21b44
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-11-11 23:23:26 +1100

    updated with true/false and removed odd/even

--- goblin_hunt.py
+++ goblin_hunt.py
@@ -5,14 +5,12 @@ def goblin_hunt():
     print("Stephen, do you think you can find the goblin hiding in the kitchen cupboards?")
     print("|_||_||_||_||_|")
     i=0
-    while i<=5:
+    while True:
           val=int(input("Which cupboard do you think the goblin is in [type in number]:"))
           if val==3:
-            i=1
+            False
             print("Well done!! You have found the goblin. He was so scared he ran away.")
             break
           else:
             print("Sorry! The goblin is still lurking somewhere else")
-            i=i+1
-
 goblin_hunt()
--- print_odd_numbers.py
+++ print_odd_numbers.py
@@ -13,22 +13,4 @@ def odd_number():
             val = val + 2
         print(val)
 
-def odd_number_using_while():
-    print("Enter a number:")
-    val = int(input())
-    print("You entered : '<{0}>'".format(val))
-    print("How many odd numbers you wish to print?:")
-    pnum = int(input())
-    out = 0
-    print("Printing {0} odd numbers after {1}:".format(pnum, val))
-
-    while out < pnum:
-        if val % 2 == 0:
-            val = val + 1
-        else:
-            val = val + 2
-        print(val)
-        out = out + 1
-
 odd_number()
-odd_number_using_while()
