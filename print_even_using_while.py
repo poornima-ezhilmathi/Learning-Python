3d68ee5c7d4c805fd404d37c96a65fc2ece0fdab
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-11-11 23:27:14 +1100

    updated on while loop to print even numbers

---
+++ print_even_using_while.py
@@ -0,0 +1,17 @@
+def even_number_using_while():
+    print("Enter a number:")
+    val = int(input())
+    print("You entered : '<{0}>'".format(val))
+    print("How many even numbers you wish to print?:")
+    pnum = int(input())
+    out = 0
+    print("Printing {0} even numbers after {1}:".format(pnum, val))
+    while out < pnum:
+        if val %2 != 0:
+            val = val + 1
+        else:
+            val = val + 2
+        print(val)
+        out=out+1
+
+even_number_using_while()
