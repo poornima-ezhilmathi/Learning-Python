25da53d33a09c2e7a340a63c004a7302c7539411
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-11-18 15:36:34 +1100

    list indexing

---
+++ list_indexing_operations.py
@@ -0,0 +1,20 @@
+def lst_ind_operations():
+    print("Enter a list with value:")
+    lst_val=list(input())
+    print("You entered : '{0}'".format(lst_val))
+    print("--------------------------------------------")
+    print("First item in '{1}' is '{0}' " .format(lst_val[0],lst_val))
+    print("--------------------------------------------")
+    print(len(lst_val))
+    lst_str = len(lst_val) -1
+    print("Last item in '{1}' is '{0}'" .format(lst_val[lst_str],lst_val))
+    print("--------------------------------------------")
+    print("Length of list  '{1}' is  '{0}'" .format(len(lst_val),lst_val))
+    print("--------------------------------------------")
+    mid_index=len(lst_val)//2
+    print("Mid index is '{0}'" .format(mid_index))
+    print("--------------------------------------------")
+    # if the string is even then return two character otherwise one character
+    print("Middle Character in '{1}' is/are {0}". format(lst_val[(len(lst_val)-1)//2:(len(lst_val)+2)//2],lst_val))
+
+lst_ind_operations()
