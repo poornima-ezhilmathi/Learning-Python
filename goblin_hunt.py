2cc8b1dbd1e10f62f0cbdd7a9e3c69ab7a2d1a6d
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-11-18 21:56:46 +1100

    goblin updated

--- goblin_hunt.py
+++ goblin_hunt.py
@@ -1,12 +1,18 @@
+import random
 def goblin_hunt():
     print("Welcome to the Angry Goblin Hunt")
     print("An award-winning game full of adventure and excitement (!)")
     input("Type in your name:")
     print("Stephen, do you think you can find the goblin hiding in the kitchen cupboards?")
-    print("|_||_||_||_||_|")
+    num=int(input("Input the level of difficulty"))
+    cupboard="|_|"
+    cupboards = (num * cupboard)
+    print(cupboards)
     while True:
           val=int(input("Which cupboard do you think the goblin is in [type in number]:"))
-          if val==3:
+          usernum=random.randint(1, num)
+          print(usernum)
+          if val == usernum:
             print("Well done!! You have found the goblin. He was so scared he ran away.")
             break
           else:
