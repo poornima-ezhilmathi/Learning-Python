01034edc0bd48622ccf281d39df6800446174ddf
poornima-ezhilmathi <poornima-ezhilmathi@example.com>
2022-12-08 20:24:45 +1100

    reading a list using map function

--- list_slicing_operations.py
+++ list_slicing_operations.py
@@ -1,5 +1,7 @@
 def lst_slicing_operations():
-    lst_val = ["Apple", "Orange", "Kiwi", [1, 2, 3]]
+    lst_val = int(input("Enter number of elements to insert into the list: "))
+    lst_val = list(map(int, input("\nEnter the numbers separated by comma(ex: 222,55,77) : ").strip().split(',')))[
+              :lst_val]
     print("You entered : '{0}'".format(lst_val))
     print("--------------------------------------------")
     print("Slice of '{1}' after removing first and last items '{0}' " .format(lst_val[1:-1],lst_val))
